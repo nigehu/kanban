51afc1e77ba4e07a43798afc45d4ac1d142ba358
Nigel Hughes <nigelhughes00@gmail.com>
2021-10-07 14:57:39 -0700

    intial commit for react

--- kanbanboard/settings.py
+++ kanbanboard/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'playground',
     'api.apps.ApiConfig',
-    'rest_framework'
+    'rest_framework',
+    'frontend.apps.FrontendConfig'
 ]
 
 MIDDLEWARE = [
