f15b564bdf04b307d74c4ca7bf0246ed41a8e8d0
Nigel Hughes <nigelhughes00@gmail.com>
2021-10-12 12:16:38 -0700

    set up separate pages for conent in react router

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,10 +1,12 @@
 import { Box, Tab, Tabs } from "@mui/material";
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Route, Switch, useHistory } from "react-router-dom";
 import HomePage from "./home/HomePage";
+import KanbanBoard from "./kanban/KanbanBoard";
 
 function App() {
-  const [selectedTab, setSelectedTab] = useState("home");
+  const history = useHistory();
+  const [selectedTab, setSelectedTab] = useState("/");
 
   function a11yProps(tabGroup: string, id: string) {
     return {
@@ -13,36 +15,38 @@ function App() {
     };
   }
 
-  function handleTabChange() {}
+  function handleTabChange(value: string) {
+    setSelectedTab(value);
+    history.push(value);
+  }
 
   return (
     <div>
       <Box sx={{ height: 50, width: "100vw", backgroundColor: "red" }}>
         <Tabs
           value={selectedTab}
-          onChange={handleTabChange}
+          onChange={(e, v) => handleTabChange(v)}
           aria-label="navigation tabs"
         >
-          <Tab label="home" value="home" {...a11yProps("nav", "home")} />
-          <Tab label="users" value="users" {...a11yProps("nav", "users")} />
+          <Tab label="home" value="/" {...a11yProps("nav", "home")} />
+          <Tab label="users" value="/users" {...a11yProps("nav", "users")} />
+          <Tab label="kanban" value="/kanban" {...a11yProps("nav", "kanban")} />
         </Tabs>
       </Box>
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <HomePage />
-          </Route>
-          <Route path="/kanban">
-            <h1>React Kanban Board!</h1>
-          </Route>
-          <Route path="/users/:id">
-            <p>test</p>
-          </Route>
-          <Route path="*">
-            <h1>Yikes! No page found here...</h1>
-          </Route>
-        </Switch>
-      </Router>
+      <Switch>
+        <Route exact path="/">
+          <HomePage />
+        </Route>
+        <Route path="/kanban">
+          <KanbanBoard />
+        </Route>
+        <Route path="/users">
+          <p>test</p>
+        </Route>
+        <Route path="*">
+          <h1>Yikes! No page found here...</h1>
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ frontend/src/components/kanban/KanbanBoard.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function KanbanBoard() {
+  return <h1>KANBAN!</h1>;
+}
